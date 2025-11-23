c7fac5df5a0f7441bd1068067f4b161f2751f8bb
callmeaash <aashishbogati9@gmail.com>
2025-11-23 23:03:16 +0545

    remove public

--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-require_once '../includes/db.php';
-require_once '../includes/models.php';
-
-$search = $_GET['search'] ?? null;
-$items = Item::getActiveItems($search);
-echo json_encode($items);
-?>
